ca88485053c7b3546f9b6fff9090a729f96f66e8
jp-cardoso <jhonpoal@hotmail.com>
2024-06-08 10:29:43 -0300

    style: inicio da estilização do component left panel

---
+++ src/app/components/left-panel/left-panel.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-left-panel',
+  standalone: true,
+  imports: [
+    MatButtonModule
+  ],
+  templateUrl: './left-panel.component.html',
+  styleUrl: './left-panel.component.scss'
+})
+export class LeftPanelComponent {
+
+  onClickPage() {
+    
+  }
+
+}
--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { LeftPanelComponent } from '../../components/left-panel/left-panel.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    LeftPanelComponent
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
