8564342577bb6fa8d542aa426285ef33ee2e1401
jp-cardoso <jhonpoal@hotmail.com>
2024-06-08 11:13:19 -0300

    style: estilização da conta do cliente

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { LeftPanelComponent } from '../../components/left-panel/left-panel.component';
+import { TransactionsComponent } from '../../components/transactions/transactions.component';
+import { CardComponent } from '../../components/card/card.component';
+import { CardInfoComponent } from '../../components/card-info/card-info.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [
-    LeftPanelComponent
+    LeftPanelComponent,
+    TransactionsComponent,
+    CardComponent,
+    CardInfoComponent
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
