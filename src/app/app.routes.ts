8e8fc872f8aabe84540dead403ba1d39efaec662
jp-cardoso <jhonpoal@hotmail.com>
2024-06-09 16:30:18 -0300

    feat: .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,5 +21,9 @@ export const routes: Routes = [
   {
     path: "about",
     loadComponent: () => import("./pages/about/about.component").then(m => m.AboutComponent)
+  }, 
+  {
+    path: "home",
+    redirectTo: ""
   }
 ];
