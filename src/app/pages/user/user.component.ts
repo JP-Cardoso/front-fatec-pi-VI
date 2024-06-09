c664be1540a814664bc516d25d32de18c0d191a3
jp-cardoso <jhonpoal@hotmail.com>
2024-06-08 22:24:58 -0300

    feat: mudança de componente

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LeftPanelComponent } from '../../components/left-panel/left-panel.component';
-import { TransactionsComponent } from '../../components/transactions/transactions.component';
+import { TransactionsComponent } from '../../components/user/transactions/transactions.component';
 import { CardComponent } from '../../components/card/card.component';
 import { CardInfoComponent } from '../../components/card-info/card-info.component';
 import { CardAccountComponent } from '../../components/user/card-account/card-account.component';
