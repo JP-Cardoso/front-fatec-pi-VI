fda8169c93ebe78875d3322ebbfb94de8ceb3a69
jp-cardoso <jhonpoal@hotmail.com>
2024-06-09 13:17:47 -0300

    feat: .

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router } from '@angular/router';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    MatToolbarModule
+    MatToolbarModule,
+    IconComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  private logo: string = "/src/assets/icons/icon-a.svg"
 
   private router: Router = inject(Router);
+
+  ngOnInit() {
+    console.log(this.logo);
+
+  }
 
   onSingUp() {
     this.router.navigateByUrl("/create-account");
