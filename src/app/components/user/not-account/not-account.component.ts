12dccae3091e620b6975669138857d09324cf978
jp-cardoso <jhonpoal@hotmail.com>
2024-06-09 00:26:15 -0300

    style: estilização de formulário de não tem conta

--- src/app/components/user/not-account/not-account.component.ts
+++ src/app/components/user/not-account/not-account.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { NotAccountFormComponent } from '../not-account-form/not-account-form.component';
 
 @Component({
   selector: 'app-not-account',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    NotAccountFormComponent],
   templateUrl: './not-account.component.html',
   styleUrl: './not-account.component.scss'
 })
 export class NotAccountComponent {
+
+  status: boolean = true;
+
+  openForm(status: boolean) {
+    this.status = status;
+  }
+
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
